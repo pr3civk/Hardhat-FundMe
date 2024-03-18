13834f47b65a02d025f4045009946877109242d9
Piotr Preciuk <piotr.preciukbusiness@gmail.com>
2024-03-18 21:55:50 +0100

    Created new Hardhat app for FundMe contract from freeCodeCamp

---
+++ contracts/FundMe.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+error NotOwner();
+error NotEnoughFunds(uint256 required, uint256 available);
+
+contract FundMe{
+    //
+}
---
+++ hardhat.config.ts
@@ -0,0 +1,32 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "dotenv/config";
+import "@nomicfoundation/hardhat-toolbox-viem";
+
+export const ETHERSCAN_API_KEY: string = process.env.ETHERSCAN_API_KEY!;
+const ALCHEMY_SEPOLIA_RPC_URL: string = process.env.ALCHEMY_SEPOLIA_RPC_URL!;
+const PRIVATE_KEY: string = process.env.PRIVATE_KEY!;
+
+const config: HardhatUserConfig = {
+    solidity: "0.8.24",
+    networks: {
+        sepolia: {
+            url: ALCHEMY_SEPOLIA_RPC_URL,
+            accounts: [PRIVATE_KEY],
+            chainId: 11155111,
+        },
+        hardhat: {
+            chainId: 31337,
+        },
+        localhost: {
+            chainId: 31337,
+            url: "http://127.0.0.1:8545",
+        },
+    },
+    etherscan: {
+        apiKey: {
+            sepolia: ETHERSCAN_API_KEY,
+        },
+    },
+};
+
+export default config;
